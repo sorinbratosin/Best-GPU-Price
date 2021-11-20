e9a5b0383a51ec58329e88a59741908425db6b1d
sorinbratosin <sorinbratosin93@yahoo.com>
2021-11-20 11:05:52 +0200

    added functionality to the extract button. It waits for both crawlers to be done before displaying all GPUs in the GUI

--- src/main/java/com/sorinbratosin/BestGPUPrice/Crawler/CrawlerManager.java
+++ src/main/java/com/sorinbratosin/BestGPUPrice/Crawler/CrawlerManager.java
@@ -2,15 +2,26 @@ package com.sorinbratosin.BestGPUPrice.Crawler;
 
 public class CrawlerManager {
 
+    private Thread emagThread;
+    private Thread pcGarageThread;
+
     public void crawl() {
 
         EmagCrawler emagCrawler = new EmagCrawler();
         PcGarageCrawler pcGarageCrawler = new PcGarageCrawler();
 
-        Thread emagThread = new Thread(emagCrawler);
-        Thread pcGarageThread = new Thread(pcGarageCrawler);
+        emagThread = new Thread(emagCrawler);
+        pcGarageThread = new Thread(pcGarageCrawler);
 
         emagThread.start();
         pcGarageThread.start();
+    }
+
+    public Thread getEmagThread() {
+        return emagThread;
+    }
+
+    public Thread getPcGarageThread() {
+        return pcGarageThread;
     }
 }
--- src/main/java/com/sorinbratosin/BestGPUPrice/Main.java
+++ src/main/java/com/sorinbratosin/BestGPUPrice/Main.java
@@ -1,29 +1,11 @@
 package com.sorinbratosin.BestGPUPrice;
-
-import com.sorinbratosin.BestGPUPrice.Crawler.PcGarageCrawler;
-import com.sorinbratosin.BestGPUPrice.Database.GPU;
 import com.sorinbratosin.BestGPUPrice.View.MainWindow;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        /*EmagCrawler emagCrawler = new EmagCrawler();
-        List<GPU> gpuList = emagCrawler.extract(); */
-
-        /*int x = 0;
-        for(GPU gpu : gpuList) {
-            System.out.println(++x + ". " + gpu.getName() + "\n" + "Price: " + gpu.getPrice() + "; Available: " + gpu.isAvailable() + "; URL: " + gpu.getUrl());
-        } */
-
-        /*PcGarageCrawler pcGarageCrawler = new PcGarageCrawler();
-
-            List<GPU> pcGarageGpuList = pcGarageCrawler.getAllGPU();
-            int x = 0;
-            for (GPU gpu : pcGarageGpuList) {
-                System.out.println(++x + ". " + gpu.getName() + "\n" + "Price: " + gpu.getPrice() + "; Available: " + gpu.isAvailable() + "; URL: " + gpu.getUrl());
-            }*/
 
         MainWindow mainWindow = new MainWindow();
+        mainWindow.createFrame();
         }
     }
